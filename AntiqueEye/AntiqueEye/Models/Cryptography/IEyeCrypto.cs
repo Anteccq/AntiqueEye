7ade7624c98c412fa1861f05dec95d5116144b35
AntiqueR <antique.pendulum@gmail.com>
2021-03-08 03:31:48 +0900

    add IEyeCrypto interface

---
+++ AntiqueEye/AntiqueEye/Models/Cryptography/IEyeCrypto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiqueEye.Models.Cryptography
+{
+    public interface IEyeCrypto
+    {
+        public byte[] EncryptAsync(string password, Message message);
+        public Message DecryptAsync(string password, byte[] encryptedData);
+    }
+}
