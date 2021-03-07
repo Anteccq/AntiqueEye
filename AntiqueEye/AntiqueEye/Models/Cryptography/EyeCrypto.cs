99d5161d26ee5e3fa6733e15ea6d9b6be3eaafa2
AntiqueR <antique.pendulum@gmail.com>
2021-03-08 05:46:39 +0900

    changed: these methods to be asynchronous

--- AntiqueEye/AntiqueEye/Models/Cryptography/EyeCrypto.cs
+++ AntiqueEye/AntiqueEye/Models/Cryptography/EyeCrypto.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AntiqueEye.Models.Cryptography
@@ -13,27 +14,29 @@ namespace AntiqueEye.Models.Cryptography
         private const int saltSize = 16;
         private const int keySize = 16;
 
-        public byte[] DecryptAsync(string password, Message message)
+        public async Task<byte[]> DecryptAsync(string password, Message message, CancellationToken cancellationToken = default)
         {
+            cancellationToken.ThrowIfCancellationRequested();
             using var deriveBytes = new Rfc2898DeriveBytes(password, message.Salt);
             using var decAlg = Aes.Create();
             decAlg.Key = deriveBytes.GetBytes(keySize);
             decAlg.IV = message.Iv;
             using var ms = new MemoryStream();
             using var cs = new CryptoStream(ms, decAlg.CreateDecryptor(), CryptoStreamMode.Write);
-            cs.Write(message.EncryptedData);
+            await cs.WriteAsync(message.EncryptedData, cancellationToken);
             return ms.ToArray();
         }
 
-        public Message EncryptAsync(string password, byte[] rawData)
+        public async Task<Message> EncryptAsync(string password, byte[] rawData, CancellationToken cancellationToken = default)
         {
+            cancellationToken.ThrowIfCancellationRequested();
             using var deriveBytes = new Rfc2898DeriveBytes(password, saltSize);
             var salt = deriveBytes.Salt;
             using var encAlg = Aes.Create();
             encAlg.Key = deriveBytes.GetBytes(keySize);
             using var ms = new MemoryStream();
             using var cs = new CryptoStream(ms, encAlg.CreateEncryptor(), CryptoStreamMode.Write);
-            cs.Write(rawData);
+            await cs.WriteAsync(rawData, cancellationToken);
             var message = new Message
             {
                 EncryptedData = ms.ToArray(),
--- AntiqueEye/AntiqueEye/Models/Cryptography/IEyeCrypto.cs
+++ AntiqueEye/AntiqueEye/Models/Cryptography/IEyeCrypto.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AntiqueEye.Models.Cryptography
 {
     public interface IEyeCrypto
     {
-        public Message EncryptAsync(string password, byte[] rawData);
-        public byte[] DecryptAsync(string password, Message message);
+        public Task<Message> EncryptAsync(string password, byte[] rawData, CancellationToken cancellationToken = default);
+        public Task<byte[]> DecryptAsync(string password, Message message, CancellationToken cancellationToken = default);
     }
 }
