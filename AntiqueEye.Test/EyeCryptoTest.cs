d108d57abf5c40e508bf777e63e6ce0fd57e3eaf
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 04:42:07 +0900

    The end of the "Unit Test" war  within.
    取り消せよ……!!!　ハァ…　今の言葉……!!!

---
+++ AntiqueEye.Test/EyeCryptoTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntiqueEye.Models.Cryptography;
+using Xunit;
+
+namespace AntiqueEye.Test
+{
+    public class EyeCryptoTest
+    {
+        [Theory]
+        [InlineData("123456789", "passw0rd")]
+        [InlineData("123456789 ", "p@ssW0r$")]
+        [InlineData("abcdefghijk ", "123456789")]
+        [InlineData("AntiqueR ", "Pendulum")]
+        [InlineData("", "")]
+        public void EncryptDecryptTest(string rawData, string password)
+        {
+
+        }
+    }
+}
