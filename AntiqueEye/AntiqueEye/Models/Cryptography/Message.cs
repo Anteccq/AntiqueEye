ca93cd91580290e3d656837d086895bbc8356262
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 23:33:48 +0900

    add MessagePackAttribute

--- AntiqueEye/AntiqueEye/Models/Cryptography/Message.cs
+++ AntiqueEye/AntiqueEye/Models/Cryptography/Message.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessagePack;
 
 namespace AntiqueEye.Models.Cryptography
 {
+    [MessagePackObject]
     public class Message
     {
+        [Key(0)]
         public byte[] EncryptedData { get; set; }
+        [Key(1)]
         public byte[] Iv { get; set; }
+        [Key(2)]
         public byte[] Salt { get; set; }
     }
 }
