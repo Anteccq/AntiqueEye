7cb13c5edac59d1a227cecbeaa0f72117f5498d4
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 20:58:38 +0900

    add Encrypt Test (encrypt must always create different byte[] data)

--- AntiqueEye.Test/EyeCryptoTest.cs
+++ AntiqueEye.Test/EyeCryptoTest.cs
@@ -26,5 +26,23 @@ namespace AntiqueEye.Test
             var actualText = Encoding.UTF8.GetString(decryptedData);
             actualText.Is(rawText);
         }
+
+        [Fact]
+        public async Task EncryptAlwaysCreateDifferentData()
+        {
+            var rawText = "planeText";
+            var password = "password";
+            var rawData = Encoding.UTF8.GetBytes(rawText);
+            var cry = new EyeCrypto();
+            var encrypt1 = await cry.EncryptAsync(password, rawData);
+            var encrypt2 = await cry.EncryptAsync(password, rawData);
+            encrypt2.EncryptedData.IsNot(encrypt1.EncryptedData);
+
+            var decrypt1 = await cry.DecryptAsync(password, encrypt1);
+            var decrypt2 = await cry.DecryptAsync(password, encrypt2);
+            var actualText1 = Encoding.UTF8.GetString(decrypt1);
+            var actualText2 = Encoding.UTF8.GetString(decrypt2);
+            actualText1.Is(actualText2);
+        }
     }
 }
