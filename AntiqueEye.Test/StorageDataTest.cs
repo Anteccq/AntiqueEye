767e1635e3464b024da265c77ec714889b9bdced
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 23:43:44 +0900

    add ReadTest

---
+++ AntiqueEye.Test/StorageDataTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntiqueEye.Models;
+using AntiqueEye.Models.Cryptography;
+using MessagePack;
+using Xunit;
+using static System.Text.Encoding;
+
+namespace AntiqueEye.Test
+{
+    public class StorageDataTest
+    {
+        [Fact]
+        public async Task ReadTest()
+        {
+            var message = new Message()
+            {
+                EncryptedData = UTF8.GetBytes("EncryptedData"),
+                Iv = UTF8.GetBytes("Iv"),
+                Salt = UTF8.GetBytes("Salt")
+            };
+
+            string fileName;
+            await using (var fs = new FileStream("./testfile", FileMode.Create, FileAccess.Write))
+            {
+                await MessagePackSerializer.SerializeAsync(fs, message);
+                fileName = fs.Name;
+            }
+
+            var sd = new StorageData();
+            var result = await sd.ReadAsync(fileName);
+            result.EncryptedData.Is(message.EncryptedData);
+            result.Iv.Is(message.Iv);
+            result.Salt.Is(message.Salt);
+        }
+    }
+}
