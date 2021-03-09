e5460a657e1fe6c4c42a0bade83b8baa13c0813d
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 21:33:40 +0900

    add storage interface

---
+++ AntiqueEye/AntiqueEye/Models/IStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntiqueEye.Models.Cryptography;
+
+namespace AntiqueEye.Models
+{
+    public interface IStorage
+    {
+        public Task<Message> ReadAsync(string path);
+
+        public Task WriteAsync(string path, Message message);
+    }
+}
