8ad02e51d5a47da4bed15dabb305d01223c616d6
AntiqueR <antique.pendulum@gmail.com>
2021-03-09 23:11:07 +0900

    add StorageData class and implemented ReadAsync method

---
+++ AntiqueEye/AntiqueEye/Models/StorageData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntiqueEye.Models.Cryptography;
+using MessagePack;
+
+namespace AntiqueEye.Models
+{
+    public class StorageData : IStorage
+    {
+        public async Task<Message> ReadAsync(string path)
+        {
+            if (!File.Exists(path)) throw new FileNotFoundException();
+
+            await using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var message = await MessagePackSerializer.DeserializeAsync<Message>(fs);
+            return message;
+        }
+
+        public Task WriteAsync(string path, Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
