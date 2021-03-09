0268d4dcc96935a2b32ffe41ea8784c8dd140d05
AntiqueR <antique.pendulum@gmail.com>
2021-03-10 00:26:04 +0900

    add IBlockModule interface

---
+++ AntiqueEye/AntiqueEye/Models/IBlockModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiqueEye.Models
+{
+    public interface IBlockModule
+    {
+        public Task<List<Block>> GetBlocksAsync();
+
+        public Task SetBlocksAsync(List<Block> blocks);
+    }
+}
