affc11259fa19ed5f019706de7c0fb3e881a6dd3
AntiqueR <antique.pendulum@gmail.com>
2021-03-10 00:42:14 +0900

    change: accept cancellation token

--- AntiqueEye/AntiqueEye/Models/IBlockModule.cs
+++ AntiqueEye/AntiqueEye/Models/IBlockModule.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AntiqueEye.Models
 {
     public interface IBlockModule
     {
-        public Task<List<Block>> GetBlocksAsync();
+        public Task<List<Block>> GetBlocksAsync(CancellationToken cancellationToken = default);
 
-        public Task SetBlocksAsync(List<Block> blocks);
+        public Task SetBlocksAsync(List<Block> blocks, CancellationToken cancellationToken = default);
     }
 }
